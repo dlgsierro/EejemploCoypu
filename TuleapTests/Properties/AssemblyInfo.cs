da969b412400902cfaaa5952154f461453909aea
dlgsierro <gsierro@dl.cl>
2017-10-24 10:12:35 -0300

    initial commit

---
+++ TuleapTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TuleapTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TuleapTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eeebeb40-8acf-4db2-90d3-1396f8ca536c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TuleapTests/TuleapTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Coypu;
+using Coypu.Drivers;
+using NUnit.Framework;
+
+namespace PruebaCoypu
+{
+    [TestFixture]
+    public class TuleapTests
+    {
+        [Test]
+        public void LoginTest()
+        {
+            var sessionConfig = new SessionConfiguration() {
+                Browser = Browser.Chrome,
+                AppHost = "dltuleap.dl.cl",
+                Timeout = TimeSpan.FromSeconds(50)
+            };
+            var browser = new BrowserSession(sessionConfig);
+            try
+            {
+                browser.Visit("");
+                browser.FindField("Username").FillInWith("gsierro");
+                browser.FindField("Password").FillInWith("Gasr3673");
+                browser.ClickButton("Login");
+            }
+            finally
+            {
+                browser.Dispose();
+            }
+        }
+    }
+}
